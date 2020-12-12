5b777a7ed6d18985d4a81472d981e04eb7228fb3
jnrobinsoniii <32677671+jnrobinsoniii@users.noreply.github.com>
2020-12-12 18:39:19 -0500

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -7,14 +7,15 @@ library(xts)
 library(tbl2xts)
 library(USAboundaries)
 
-stateList = state_codes[c(1:52,54,57,60,63),] # Get a list of names and abbreviations of states
-alphaStates = select(stateList[order(stateList$state_name),], state_name, state_abbr) # Sort alphabetically, Select name and abbreviations
+# Get an alphabetical list of names and abbreviations of states from the USAboundaries library to use for our State input and URLs
+stateList = state_codes[c(1:52,54,57,60,63),] 
+alphaStates = select(stateList[order(stateList$state_name),], state_name, state_abbr) 
 
-ui <- dashboardPage(
+ui <- dashboardPage( # Create the user interface and dashboard page using shinydashboard library
     dashboardHeader(title = "COVID-19 Timeline"),
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("Plot", tabName = "Plot", icon = icon("th")),
+    dashboardSidebar( 
+        sidebarMenu( 
+            menuItem("Plot", tabName = "Plot", icon = icon("th")), # Sidebar menu item called Plot containing 2 input sections
             selectizeInput("state", 
                            h3("State:"),
                            c("United States",
@@ -33,7 +34,9 @@ ui <- dashboardPage(
                           "Increase in Hospitalized"))
         )
     ),
-    dashboardBody(
+    dashboardBody( 
+        
+        # First row in dashboard body shows current data in Value boxes from the flexdashboard library
         fluidRow(
             column(2,flexdashboard::valueBoxOutput("vb1")),
             column(2,flexdashboard::valueBoxOutput("vb2")),
@@ -42,9 +45,13 @@ ui <- dashboardPage(
             column(2,flexdashboard::valueBoxOutput("vb5")),
             column(2,flexdashboard::valueBoxOutput("vb6"))
         ),
+        
+        # Second row in dashboard body shows time series data in a plot from the highcharter library
         fluidRow(
             column(12, highchartOutput("hcontainer"))
         ),
+        
+        # Third row in dashboard body shows rates in gauges from the flexdashboard library
         fluidRow(
             column(2,flexdashboard::gaugeOutput("gauge1")),
             column(2,flexdashboard::gaugeOutput("gauge2")),
